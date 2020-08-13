305dc21329ba89f9d0963677db5b8384fc4c289d
corifeo <pietrafilosofale@gmail.com>
2020-08-13 16:21:32 +0100

    alternative solution to reverse_vowels

--- reverse_vowels.py
+++ reverse_vowels.py
@@ -1,15 +1,25 @@
 """
 Write a function that takes a string as input and reverse only the vowels of a string.
+
 """
-VOWELS = [x for x in 'aeiou']
+VOWELS = set('aeiou')
 
 def reverse_vowels(input_string):
-    # get a list of tuples for each vowel with position and character
-    v_list = [(x, input_string[x]) for x in range(len(input_string)) if input_string[x].lower() in VOWELS]
-    # split tuples in two indexes and letters, reverse indexes and rezip them in a dict
-    r_list = dict(zip(list(zip(*v_list))[0][::-1], list(zip(*v_list))[1]))
-    # loop through input_string and replace letters based on dict
-    return ''.join([input_string[x] if x not in r_list else r_list[x] for x in range(len(input_string))])
+    string = list(input_string)
+    vowels = []
+    indexes = []
+
+    # loop through string to find vowels
+    for x, y in enumerate(string):
+        if y in VOWELS:
+            vowels.append(y)
+            indexes.append(x)
+
+    # loop through tuple and replace characters
+    for i, j in zip(indexes, reversed(vowels)):
+        string[i] = j
+
+    return ''.join(string)
 
 print(reverse_vowels('hello')) # holle
 print(reverse_vowels('lEetcode')) # leotcedE
---
+++ reverse_vowels2.py
@@ -0,0 +1,19 @@
+"""
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Alternative and more efficient solution: 500000 loops, best of 5: 846 nsec per loop
+
+"""
+VOWELS = [x for x in 'aeiou']
+
+def reverse_vowels(input_string):
+    # get a list of tuples for each vowel with position and character
+    v_list = [(x, input_string[x]) for x in range(len(input_string)) if input_string[x].lower() in VOWELS]
+    # split tuples in two indexes and letters, reverse indexes and rezip them in a dict
+    r_list = dict(zip(list(zip(*v_list))[0][::-1], list(zip(*v_list))[1]))
+    # loop through input_string and replace letters based on dict
+    return ''.join([input_string[x] if x not in r_list else r_list[x] for x in range(len(input_string))])
+
+print(reverse_vowels('hello')) # holle
+print(reverse_vowels('lEetcode')) # leotcedE
+print(reverse_vowels('anAconda')) # anocAnda
